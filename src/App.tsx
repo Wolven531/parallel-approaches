a2e37fa6beb7a23b2ef8190e02c9674104c6d8ee
wolven531 <anthony.williams.cs@gmail.com>
2022-08-03 21:08:47 -0600

    Add example comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import './App.css'
 export const App = () => {
 	const step = 200 // num msbetween each async op
 
-	const asyncOp1 = makeAsyncOp(1 * step)
-	const asyncOp2 = makeAsyncOp(2 * step)
-	const asyncOp3 = makeAsyncOp(3 * step)
-	const asyncOp4 = makeAsyncOp(4 * step)
-	const asyncOp5 = makeAsyncOp(5 * step)
-	const asyncOp6 = makeAsyncOp(6 * step)
-	const asyncOp7 = makeAsyncOp(7 * step)
-	const asyncOp8 = makeAsyncOp(8 * step)
-	const asyncOp9 = makeAsyncOp(9 * step)
+	const asyncOp1 = makeAsyncOp(1 * step) // e.g. validate some input
+	const asyncOp2 = makeAsyncOp(2 * step) // e.g. transform input
+	const asyncOp3 = makeAsyncOp(3 * step) // e.g. grab data from some other source 1
+	const asyncOp4 = makeAsyncOp(4 * step) // e.g. grab data from some other source 2
+	const asyncOp5 = makeAsyncOp(5 * step) // e.g. grab data from some other source 3
+	const asyncOp6 = makeAsyncOp(6 * step) // e.g. process input + collected data
+	const asyncOp7 = makeAsyncOp(7 * step) // e.g. upload to some other service
+	const asyncOp8 = makeAsyncOp(8 * step) // e.g. send some email / slack message
+	const asyncOp9 = makeAsyncOp(9 * step) // e.g. update some DB field to mark as processed
 
 	/**
 	 * This function demonstrates invoking a bunch of Promises using Promise.all + Array.map
@@ -99,7 +99,9 @@ export const App = () => {
 	return (
 		<article>
 			<section>
-				<p>Hello! Open the Developer Tools, and visit the console tab</p>
+				<p>
+					Hello! Open the Developer Tools, and visit the console tab
+				</p>
 			</section>
 		</article>
 	)
